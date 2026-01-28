20e0ce702d236e821e8aafeeff693dc0913c0d84
King-Forge <jak1456@gmail.com>
2026-01-28 10:40:48 -0500

    added simple cost sum functon, todo: add error checking for inputs, force 2 decimal point output

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -9,3 +9,11 @@ function calculateArea(){
 
     document.getElementById("result").innerText = `The area of the rectangle is: ${area}`;
 }
+
+function groceryTracker(){
+    let groceryTotal = parseFloat(document.getElementById("grocery1").value);
+    groceryTotal += parseFloat(document.getElementById("grocery2").value);
+    groceryTotal += parseFloat(document.getElementById("grocery3").value);
+
+    document.getElementById("groceryResult").innerText = `The total amount is: ${groceryTotal}`;
+}
